64350bf9cd1b8cd25d53d281f5a310fdaa351627
earvin saintus <you@example.com>
2023-06-29 17:24:24 +0200

    commit test

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -5,7 +5,7 @@ function Link({ uri, text }) {
 function Footer() {
   return (
     <footer>
-      <h2>More resources</h2>
+      <h2>More resourcess</h2>
       <Link uri={"https://trufflesuite.com"} text={"Truffle"} />
       <Link uri={"https://reactjs.org"} text={"React"} />
       <Link uri={"https://soliditylang.org"} text={"Solidity"} />
